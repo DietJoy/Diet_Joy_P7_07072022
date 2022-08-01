3d26c743856238732e2d3b04ae4a87ae17cb209b
DietJoy <96573392+DietJoy@users.noreply.github.com>
2022-08-01 13:31:47 +0200

    requete axios et formdata creatpost

--- frontend/my-app/src/components/CreatPost.js
+++ frontend/my-app/src/components/CreatPost.js
@@ -1,11 +1,40 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 
 const CreatPost = () => {
-const [postPicture, setPostPicture]= useState("");
+const [postPicture, setPostPicture]= useState(""); // affichage de l image dans le front
 const [postText, setPostText]= useState("");
- 
-const handlePost = () => {}; //fonction qui enverra le post dans la bdd au clic du bouton 
+const [file, setFile] = useState(); // à utliser pour l envoi de l image dans la bdd ??
+const [error, setError] = useState('');
+
+
+const handlePost = async (data)=> {
+   
+try{
+    if ( postText || postPicture){ 
+        const data = new FormData();
+        data.append('userId', data._id);
+        data.append('text', postText);
+        if (file) data.append("imageUrl", file);
+    };
+    const res = await axios.post ('http://localhost:3000/api/post',{ 
+        data
+
+});
+console.log(res);   
+
+} catch (err) {
+    setError(err.response.data?.error || err.message);
+    }
+};
+
+const handlePicture = (e) => {//fonction qui enverra l image dans la bdd au clic du bouton 
+setPostPicture(URL.createObjectURL(e.target.files[0]));
+setFile(e.target.files[0]);
+}; 
+
+
 const cancelPost = () => { //fonction qui annulera la rédaction du post
     setPostPicture("")
     setPostText("")
@@ -27,16 +56,17 @@ const cancelPost = () => { //fonction qui annulera la rédaction du post
                 id="file-upload" 
                 name="imageUrl" 
                 accept='.jpg, .jpeg, .png '
-                onChange={(e) => setPostPicture(e)} 
+                onChange={(e) => handlePicture(e)} 
                 value={postPicture} 
                 />
             </div>
             <div className='btnSend'>
-                { postText || postPicture ? ( <button className='cancel' onClick={cancelPost}>Annuler</button>) : null} // si il y a une image et un text on permet d annuler la saisi via un bouton
-            <button className='send' onClick={handlePost}>Envoyer</button>
+                { postText || postPicture ? ( <button className='cancel' onClick={cancelPost}>Annuler</button>) : null} {/*si il y a une image et un text on permet d annuler la saisi via un bouton */}
+            <button className='send' onClick={handlePost & handlePicture}>Envoyer</button>
             </div>
         </div>
     );
-};
 
-export default CreatPost;
+
+};
+export default CreatPost ;
